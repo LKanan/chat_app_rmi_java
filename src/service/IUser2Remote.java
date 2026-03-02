983cceeca2908a8dfc5ac23cf35c8ecd9a39c128
rooney <22ki129@esisalama.org>
2026-03-03 00:53:31 +0200

    fix: remove the file and directorie

--- src/service/IUser2Remote.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IUser2Remote extends Remote {
-    public void receiveMessage(String message) throws RemoteException;
-}
